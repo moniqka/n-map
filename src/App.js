14e683a69e0f3d16f9d865704a85063ba41e4ec0
Moniqka <monique.a.kowalska@gmail.com>
2018-07-10 19:15:26 +0200

    basic layout style

--- src/App.js
+++ src/App.js
@@ -5,14 +5,61 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">Wrocław Museums</h1>
+
+            <nav id="hamburger">
+              <input type="checkbox" />
+              
+              <span></span>
+              <span></span>
+              <span></span>
+              
+              <div id="menu">
+                <h2 id="list-title">best museums</h2>
+                <svg class="line" xmlns="http://www.w3.org/2000/svg" height="3px">
+                  <path class="path" d="M0 0 1200 0"/>
+                </svg>
+                <ul>
+                  <li>Show me more</li>
+                  <li>Show me more</li>
+                  <li>Show me more</li>
+                  <li>Show me more</li>
+                  <li>Show me more</li>
+
+
+                </ul>
+              </div>
+            </nav>
+        
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main id="maincontent">
+
+          <section className="search-bar" aria-label="search for museums">
+            <form>
+              <div className="search-input">
+                <input 
+
+                  type='text' 
+                  placeholder='Search for museum' 
+            
+                />
+              </div>
+            </form>
+          </section>
+          
+          <section className="map-container">
+            <div id="map" aria-label="map of museums" role="application"></div>
+          </section>
+
+
+        </main>
+
+
       </div>
     );
   }
